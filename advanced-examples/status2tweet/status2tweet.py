4001559a461cc6fbc2c47572d09b52facf453e39
Tomás Laureano Peralta Tormey <tomas.laureano.peralta.tormey@gmail.com>
2010-02-05 17:02:39 -0300

    Add license.

--- advanced-examples/status2tweet/status2tweet.py
+++ advanced-examples/status2tweet/status2tweet.py
@@ -1,3 +1,23 @@
+#!/usr/bin/jython
+#
+# Copyright 2010 t-ip: IP Development
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''This scripts post a tweet whenever you get a new call or hangup'''
+
+__author__ = 'Tomás Peralta <tomas.peralta(at)t-ip.com.ar'
+
 # GUI imports
 from javax.swing import JFrame
 from javax.swing import JPanel
